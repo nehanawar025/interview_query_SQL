7ed3adf34d6cf82c7198381002ed8c16625b1a3a
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2024-09-23 16:16:46 +0600

    Update main.sql

--- medium/Third Purchase/main.sql
+++ medium/Third Purchase/main.sql
@@ -1,1 +1,27 @@
-...
+with rank_table as(
+    select 
+        user_id,
+        created_at,
+        product_id,
+        quantity,
+        rank() over(
+            partition by user_id 
+            order by created_at, id
+            ) as purchase_rank
+    from
+        transactions
+)    
+
+select
+    user_id,
+    created_at,
+    product_id,
+    quantity
+from
+    rank_table
+where 
+    purchase_rank = 3
+
+
+
+
